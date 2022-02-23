e6802e25d838d127582e9d433651812d9cc8dbc9
JiHwonChoi <luxuryhwon@g.skku.edu>
2022-02-23 17:03:50 +0900

    0223 PropTypes를 이용해 자료형 확인하기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Food from "./Food";
+import PropTypes from 'prop-types';
 
 const foodILike =[
   {
     id:1,
     name: 'Kimchi',
     image: 'https://img.koreatimes.co.kr/upload/newsV2/images/202106/dd467848c7e349e9b456fea85ff36c1f.jpg/dims/resize/740/optimize',
-    rating:2.3
+    rating:2.3,
   },
   {
     id:2,
     name:'Namul',
     image:'https://folkency.nfm.go.kr/upload/img/20190228/20190228170057_t_.jpg',
-    rating: 3.5
+    rating: 3.5,
   },
   {
     id:3,
     name:'Ogokbob',
     image:'https://recipe1.ezmember.co.kr/cache/recipe/2020/02/06/289006ee2e01e950c528a810afba63e71.jpg',
-    rating: 4.5
+    rating: 4.5,
   }
 ]
 
@@ -28,10 +29,16 @@ function App() {
   return (
     <div className="App">
       {/* map을 사용해 여러개의 컴포넌트 한번에 만들기 */}
-      {foodILike.map(dish=>(<Food key={dish.id} name={dish.name} picture={dish.image}></Food>))}
+      {foodILike.map(dish=>(<Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}></Food>))}
       </div>
       
   )
+}
+
+Food.propTypes={
+  name:PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired,
 }
 
 export default App;
--- src/Food.js
+++ src/Food.js
@@ -4,8 +4,9 @@ function Food(props){
     return (
         <div>
             <h3>I like {props.name} very much</h3>
+            <h4>{props.rating}/5.0</h4>
             <img src={props.picture} width='200px' height='200px'></img>
-            <div height='150px'> </div>
+            <div height='150px'></div>
         </div>
         
     )
