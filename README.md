0d6309232d956bfee7e220f873e0e85a501c8220
JiHwonChoi <luxuryhwon@g.skku.edu>
2022-02-24 17:42:21 +0900

    0224 영화 api 적용하기

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #Movie app
 
 React JS Fundametals Course
+
+조금더 공부해보고 싶은 주제
+axios
+async await 동기, 비동기
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios'
 
 class App extends React.Component{
 
@@ -6,40 +7,37 @@ class App extends React.Component{
     super(props)
   }
 
-  state ={
-    count:0,
-  }
-  
-  add=()=>{
-    this.setState(current => ({
-      count: current.count +1
-    }))
+  state = {
+    isLoading: true,
+    movies: [],
   }
 
-  minus=()=>{
-    this.setState({count: this.state.count - 1})
-  }
+// 여기는 함수선언을 이렇게 하네 생활코딩이랑 다르네
+  getMovies= async ()=>{
+    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json')
 
-  render(){
-    return (
-      
-      <div>
-        <div>
-          {console.log('render')}
-          this state number: {this.state.count}
-        </div>
-        <button onClick={this.add}>+++ PUSH +++</button>
-        <button onClick={this.minus}>--- PUSH ---</button>
-      </div>
-    )
   }
 
   componentDidMount(){
-    console.log('mount')
+    // 데이터 로딩시 사용함
+
+    this.getMovies()
+    
+  //   setTimeout(()=>{
+  //     this.setState({isLoading: false})
+  //   },3000) //6000 밀리세컨드
   }
 
-  componentDidUpdate(){
-    console.log('update')
+  render(){
+
+    const {isLoading} = this.state
+    // 구조 분해할당으로 this.state 를 매번 입력하지 않아도 됨
+
+    return (
+      <div>
+        {isLoading ? 'Loading...' : 'We are ready'}
+      </div>
+    )
   }
 }
 export default App;
