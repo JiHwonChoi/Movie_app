f14404c40b2cdca734491e802e04dbc8817f2322
JiHwonChoi <luxuryhwon@g.skku.edu>
2022-03-01 17:43:34 +0900

    0301 api 적용 및받아오기

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ React JS Fundametals Course
 조금더 공부해보고 싶은 주제
 axios
 async await 동기, 비동기
+map 함수의 활용
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from 'axios'
+import Movie from './Movie'
+
 
 class App extends React.Component{
 
@@ -14,7 +16,13 @@ class App extends React.Component{
 
 // 여기는 함수선언을 이렇게 하네 생활코딩이랑 다르네
   getMovies= async ()=>{
-    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json')
+    //구조분해 할당을 통한 접근방법
+    const{
+      data:{
+        data:{movies},
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating')
+    this.setState({movies: movies, isLoading:false})
 
   }
 
@@ -30,12 +38,22 @@ class App extends React.Component{
 
   render(){
 
-    const {isLoading} = this.state
+    const {isLoading, movies} = this.state
     // 구조 분해할당으로 this.state 를 매번 입력하지 않아도 됨
 
     return (
       <div>
-        {isLoading ? 'Loading...' : 'We are ready'}
+        {isLoading ? 'Loading...' : movies.map((movie)=>{
+          
+          return <Movie
+            key={movie.id}
+            id={movie.id}
+            year ={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image}
+          ></Movie>
+        })}
       </div>
     )
   }
